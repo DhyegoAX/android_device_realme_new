5b347448dfd6f2b22cc8ce0890c5846c96f64011
DhyegoAX <dhyegocastro700@gmail.com>
2024-11-30 18:27:33 -0300

    Pica pra @miqueiasshandow999
    Change-Id: I7fadaaf068f7936f0897ef87b09a9abc7f8a52fd
    Signed-off-by: DhyegoAX <dhyegocastro700@gmail.com>

---
+++ aosp_ice.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from ice device
+$(call inherit-product, device/realme/ice/device.mk)
+
+# Inherit some common ROMs stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+PRODUCT_NAME := aosp_ice
+PRODUCT_DEVICE := ice
+PRODUCT_MANUFACTURER := realme
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3461
+
+PRODUCT_SYSTEM_NAME := RE54BFL1
+PRODUCT_SYSTEM_DEVICE := RE54BFL1
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+# Rom Specific Flags
+
+PRODUCT_NO_CAMERA := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080p
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="RMX3461-user 13 TP1A.220905.001 R.1119f23-59b8-59b9 release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+BUILD_FINGERPRINT := realme/RMX3461/RE54BFL1:13/TP1A.220905.001/R.1119f23-59b8-59b9:user/release-keys
